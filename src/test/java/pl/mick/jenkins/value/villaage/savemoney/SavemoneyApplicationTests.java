e69efe204b2d27c47524551faaf20a9b3a4f355d
f00dieveg <brzyszkiewicz@gmail.com>
2019-04-16 00:02:23 +0200

    init commit

---
+++ src/test/java/pl/mick/jenkins/value/villaage/savemoney/SavemoneyApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mick.jenkins.value.villaage.savemoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SavemoneyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
